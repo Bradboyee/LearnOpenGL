30461ede4820a71b0140cdfd74fde239ab5a58e9
Brad <bradthepparat@hotmail.com>
2024-12-18 00:19:11 +0700

    draw cube

--- code/main.cpp
+++ code/main.cpp
@@ -20,6 +20,7 @@ typedef int8_t   int8;
 typedef int16_t  int16;
 typedef int32_t  int32;
 
+#define ArrayCount(Count) sizeof(Count)/sizeof(Count[0])
 void WindowSizeCallback(GLFWwindow *Window, int Width, int Height)
 {
     glViewport(0, 0, Width, Height);
@@ -63,17 +64,67 @@ int main()
 
     // -1 to 1 and y -1 start from the bottom.
     real32 Vertices[] = {
-	// first triangle
-	// position 3 ,color 3, texture coordinate 2
-	0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f // top right
-	,  0.5f, -0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f // bottom right
-        , -0.5f, -0.5f,  0.0f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f // bottom left
-	, -0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,  1.0f // top left
+	// Front face
+	0.5f,  0.5f,  0.5f, 1.0f, 1.0f, // Top right
+	0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // Bottom right
+	-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // Bottom left
+	-0.5f,  0.5f,  0.5f, 0.0f, 1.0f, // Top left
+
+	// Back face
+	0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // Top right
+	0.5f, -0.5f, -0.5f, 1.0f, 0.0f, // Bottom right
+	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // Bottom left
+	-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // Top left
+
+	// Left face
+	-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, // Top right
+	-0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // Bottom right
+	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // Bottom left
+	-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // Top left
+
+	// Right face
+	0.5f,  0.5f,  0.5f, 1.0f, 1.0f, // Top right
+	0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // Bottom right
+	0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // Bottom left
+	0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // Top left
+
+	// Top face
+	-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // Top left
+	-0.5f,  0.5f,  0.5f, 0.0f, 0.0f, // Bottom left
+	0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // Bottom right
+	0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // Top right
+
+	// Bottom face
+	-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // Top left
+	-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // Bottom left
+	0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // Bottom right
+	0.5f, -0.5f, -0.5f, 1.0f, 1.0f  // Top right
     };
 
     uint32 Indices[] = {
-	    0, 1, 3
-	  , 1, 2, 3 
+	// Front face
+	0, 1, 3,
+	1, 2, 3,
+
+	// Back face
+	4, 5, 7,
+	5, 6, 7,
+
+	// Left face
+	8, 9, 11,
+	9, 10, 11,
+
+	// Right face
+	12, 13, 15,
+	13, 14, 15,
+
+	// Top face
+	16, 17, 19,
+	17, 18, 19,
+
+	// Bottom face
+	20, 21, 23,
+	21, 22, 23
     };
     
     uint32 Texture1;
@@ -143,20 +194,15 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 
     // NOTE(Brad): aPos
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*8, (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*5, (void*)0);
     glEnableVertexAttribArray(0);
-    // NOTE(Brad): aColor
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float)*8, (void*)(sizeof(float)*3));
-    glEnableVertexAttribArray(1);
     // NOTE(Brad): aTexCoords
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float)*8, (void*)(sizeof(float)*6));
-    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float)*5, (void*)(sizeof(float)*3));
+    glEnableVertexAttribArray(1);
     // NOTE(Brad) transformation matrix order is matter !!! scale -> rotate -> translate.
     const char *VertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
-	"layout (location = 1) in vec3 aColor;\n"
-	"layout (location = 2) in vec2 aTexCoords;\n"
-	"out vec3 VertexColor;\n"
+	"layout (location = 1) in vec2 aTexCoords;\n"
 	"out vec2 TexCoord;\n"
 	"uniform mat4 model;\n"
 	"uniform mat4 view;\n"
@@ -164,7 +210,6 @@ int main()
 	"void main()\n"
 	"{\n"
 	"	gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
-	"	VertexColor = aColor;\n"
 	"	TexCoord = aTexCoords;\n"
 	"}\0";
     
@@ -186,7 +231,6 @@ int main()
         
     const char *FragmentShaderSource1 = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"in vec3 VertexColor;\n"
 	"in vec2 TexCoord;\n"
 	"uniform sampler2D UniTexture1;\n"
 	"uniform sampler2D UniTexture2;\n"
@@ -194,39 +238,20 @@ int main()
 	"{\n"
 	"       FragColor = mix(texture(UniTexture1, TexCoord), texture(UniTexture2, TexCoord), 0.2);\n"
 	"}\0";
-    
-    const char *FragmentShaderSource2 = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"uniform vec4 UniColor;\n"
-	"void main()\n"
-	"{\n"
-	"       FragColor = vec4(UniColor.r, UniColor.g, UniColor.b, UniColor.a);\n"
-	"}\0";
 
     uint32 FragmentShader1;
     FragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
-    uint32 FragmentShader2;
-    FragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
 
     glShaderSource(FragmentShader1, 1, &FragmentShaderSource1, 0);
     glCompileShader(FragmentShader1);
-    
-    glShaderSource(FragmentShader2, 1, &FragmentShaderSource2, 0);    
-    glCompileShader(FragmentShader2);    
 
     uint32 ShaderProgram1;
     ShaderProgram1 = glCreateProgram();
-    uint32 ShaderProgram2;
-    ShaderProgram2 = glCreateProgram();
     
     glAttachShader(ShaderProgram1, VertexShader);    
     glAttachShader(ShaderProgram1, FragmentShader1);
-    
-    glAttachShader(ShaderProgram2, VertexShader);    
-    glAttachShader(ShaderProgram2, FragmentShader2);
         
     glLinkProgram(ShaderProgram1);
-    glLinkProgram(ShaderProgram2);
 
     glGetProgramiv(ShaderProgram1, GL_LINK_STATUS, &Success);
     if(!Success)
@@ -235,29 +260,16 @@ int main()
 	std::cout << "ERROR::SHADER1::PROGRAM::STATUS_FAILED" << InfoLog << std::endl;
     }
     
-    glGetProgramiv(ShaderProgram2, GL_LINK_STATUS, &Success);
-    if(!Success)
-    {
-	glGetProgramInfoLog(ShaderProgram2, 512, 0, InfoLog);
-	std::cout << "ERROR::SHADER2::PROGRAM::STATUS_FAILED" << InfoLog << std::endl;
-    }
-    
     //NOTE(Brad): can delete obj.
     glDeleteShader(VertexShader);
     glDeleteShader(FragmentShader1);
-    glDeleteShader(FragmentShader2);
-
-    int UniColorLocation = glGetUniformLocation(ShaderProgram2, "UniColor");
-    if(UniColorLocation == -1)
-    {
-	std::cout << "get UniColorLocation FAILED" << std::endl;
-    }
 
     //NOTE(Brad): set location sampler2d.
     glUseProgram(ShaderProgram1);
     glUniform1i(glGetUniformLocation(ShaderProgram1, "UniTexture1"), 0);
     glUniform1i(glGetUniformLocation(ShaderProgram1, "UniTexture2"), 1);        
-    
+
+    glEnable(GL_DEPTH_TEST); 
     while(!glfwWindowShouldClose(Window))
     {
         // NOTE(Brad): input.
@@ -267,7 +279,7 @@ int main()
         // NOTE(Brad): state setting. 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         // NOTE(Brad): state using.
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// NOTE(Brad): select VAO to draw.
 	glBindVertexArray(VAO);
@@ -277,19 +289,17 @@ int main()
 	glBindTexture(GL_TEXTURE_2D, Texture1);
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, Texture2);
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
 	real32 TimeValue = glfwGetTime();
 	real32 tValue = (sin(TimeValue * 2) / 2.0f) + 0.5f;
 	//glUseProgram(ShaderProgram2);
 	// NOTE(Brad): assign value to uniform.
-	glUniform4f(UniColorLocation, tValue, 0.2f, 0.0f, 1.0f);
 
 	glm::mat4 view = glm::mat4(1.0f);// transform local -> world space matrix.
 	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 	glm::mat4 model = glm::mat4(1.0f);// transform world -> view space matrix.
-	model = glm::rotate(model, TimeValue * glm::radians(50.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::rotate(model, TimeValue * glm::radians(50.0f), glm::vec3(1.0f, 1.0f, 0.0f));
 	
 	glm::mat4 projection = glm::mat4(1.0f);// view space -> cliped space matrix.	
 	projection = glm::perspective(glm::radians(45.0f), (real32)width/(real32)height, 0.1f, 100.0f);
@@ -302,7 +312,7 @@ int main()
 	glUniformMatrix4fv(ViewUniLocation, 1, GL_FALSE, glm::value_ptr(view));
 	glUniformMatrix4fv(ProjectionUniLocation, 1, GL_FALSE, glm::value_ptr(projection));
 	
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(sizeof(uint32)*3));
+	glDrawElements(GL_TRIANGLES, ArrayCount(Indices), GL_UNSIGNED_INT, 0);
 
 	// NOTE(Brad): remove VAO to draw.
 	glBindVertexArray(0);		
@@ -317,7 +327,6 @@ int main()
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
     glDeleteProgram(ShaderProgram1);
-    glDeleteProgram(ShaderProgram2);
     glDeleteTextures(1, &Texture1);
     glDeleteTextures(1, &Texture2);
     glfwTerminate();
