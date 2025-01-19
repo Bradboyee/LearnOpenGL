8b3d33cbfec90771b3d20d8542054976ff03d75d
Brad <bradthepparat@hotmail.com>
2025-01-19 22:04:45 +0700

    build output name for debugger

--- code/build.sh
+++ code/build.sh
@@ -1,4 +1,4 @@
 mkdir ../build
 pushd ../build
-clang++ -g ../code/main.cpp ../code/glad.c -I../include -L/opt/homebrew/lib -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit 
+clang++ -g ../code/main.cpp -o main ../code/glad.c -I../include -L/opt/homebrew/lib -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit 
 popd
